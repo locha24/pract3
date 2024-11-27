06d7998d26e553330207684791f0ff70eb2cccd1
Locha <lbol8340@gmail.com>
2024-11-27 11:17:54 +0200

    Commit backup

--- pub/script.js
+++ pub/script.js
@@ -1,11 +1,44 @@
 'use strict';
 
-let js = "amazing";
+const itemsList = document.getElementById('items-list');
+const addItemButton = document.getElementById('add-item');
+const priceInput = document.getElementById('price');
+const categorySelect = document.getElementById('category');
+const totalPrice = document.getElementById('total-price');
 
-console.log(js);
+const categoriesPrices = {
+    'hamburger': 50,
+    'potato': 30,
+    'drinks': 20,
+    'meat': 100
+};
 
-js = "23";
-console.log(js);
+const categoriesImages = { 
+    'hamburger': './images/hamburger.jpg', 
+    'potato': './images/potato.jpg', 
+    'drinks': './images/drinks.jpg', 
+    'meat': './images/meat.jpg' 
+}
 
-js=[];
-console.log(js);
+categorySelect.addEventListener('change', () => {
+    const selectedCategory = categorySelect.value;
+    priceInput.value = categoriesPrices[selectedCategory];
+});
+
+addItemButton.addEventListener('click', () => {
+    const priceValue = priceInput.value;
+    const categoryValue = categorySelect.value;
+    const imageUrl = categoriesImages[categoryValue];
+
+    if (priceValue && categoryValue) {
+        const itemblock = document.createElement('li');
+        itemblock.classList.add('p-2', 'rounded-lg', 'flex', 'justify-between', 'items-center', 'mb-2');
+
+        const itemImage = document.createElement('img');
+        itemImage.src = imageUrl;
+        itemImage.alt = categoryValue;
+        itemImage.classList.add('h-12', 'w-12', 'rounded-full');
+    }
+
+    console.log('click');
+})
